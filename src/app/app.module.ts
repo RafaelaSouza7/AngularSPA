608169341282627326d14edf73724ca8ca85b861
Rafaela Souza <Rafaelasouza2207@hotmail.com>
2023-12-10 22:43:27 -0300

    Implementação da aplicação SPA com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import {rootRouterConfig} from './app.routes'
+import { ProdutoService } from './produtos/produtos.service';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    SobreComponent,
+    ContatoComponent,
+    ListaProdutoComponent,
+    DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    ProdutoService,
+    {provide:APP_BASE_HREF, useValue:'/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
